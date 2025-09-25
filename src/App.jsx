cec42b3e715ef3c95f2d6237415163f7122ca606
lam thanh qui <lamthanhquihgs@gmail.com>
2025-09-25 18:02:00 +0700

    #1 login register forgot password home  profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Login from "./Pages/Auth/Login";
+import RegisterPage from "./Pages/Auth/RegisterPage";
+import ForgotPassword from "./Pages/Auth/ForgotPassword";
+import StaffLayout from "./Layout/StaffLayout";
+import StaffRoutes from "./Routes/StaffRoutes";
 
 function App() {
   const router = createBrowserRouter([
     { path: '/login', element: <Login /> },
-    { path: '/', element: <Login /> },
+    { path: '/register', element: <RegisterPage /> },
+    { path: '/forgot-password', element: <ForgotPassword /> },
+    {
+      element: <StaffLayout />,
+      children: StaffRoutes
+    }
   ])
 
-  return <RouterProvider  router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App
